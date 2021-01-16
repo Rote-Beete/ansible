aa45685d67cededf77652cb2723b4cd245ef4138
Michael Gerlach <n3ph@cccfr.de>
2021-01-16 20:07:03 +0100

    relocate working dir

--- utils/import_ssh_hostkey.sh
+++ utils/import_ssh_hostkey.sh
@@ -4,6 +4,10 @@
 PWD=$(dirname "$(readlink -f "$0")")
 source "${PWD}/common.sh"
 
+# change directory
+OLD_DIR=${pwd}
+cd "$PWD/.."
+
 # get FQDN of host
 read -r -p 'hostname: ' fqdn
 
@@ -13,4 +17,7 @@ host -N 0 "$fqdn" 2>&1>/dev/null \
 
 # add ssh public hostkey to inventory hosts 
 ansible_ssh_hostkey_pub=$(ssh-keyscan -t ed25519 -H "$fqdn" 2>/dev/null )
-yq e ".all[\"hosts\"][\"$fqdn\"].ansible_ssh_hostkey_pub=\"$ansible_ssh_hostkey_pub\"" -i "${PWD}/../inventory.yml"
+yq e ".all[\"hosts\"][\"$fqdn\"].ansible_ssh_hostkey_pub=\"$ansible_ssh_hostkey_pub\"" -i "inventory.yml"
+
+# change directory
+cd $OLD_PWD
